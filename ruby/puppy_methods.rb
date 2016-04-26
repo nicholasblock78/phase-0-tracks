39fda8d4d89d9aee7ab9b3970be303478f2278fe
Nick Block <nicholasblock78@gmail.com>
2016-04-26 18:24:59 -0500

    6.2 Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	puts "Woof" * integer
+  end
+
+  def roll_over
+  	puts "*roll over*"
+  end
+
+  def dog_years(human_years)
+  	p human_years.to_i * 7 
+  end
+
+  def shake
+  	puts "Shakes water everywhere!"
+  end
+
 end
 
+Fido = Puppy.new
+Fido.fetch('ball')
+Fido.speak(3)
+Fido.roll_over
+Fido.dog_years(4)
+Fido.shake
