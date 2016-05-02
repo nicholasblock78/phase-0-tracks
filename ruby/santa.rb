0ad254897f119377de0dca5e4d1672abf2c725dc
Nick Block <nicholasblock78@gmail.com>
2016-05-01 22:46:01 -0500

    6.3 Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,26 +1,62 @@
 class Santa
+
 	def initialize(gender, ethnicity)
 		p "Initializing Santa instance..."
-		@gender, @ethnicity = gender, ethnicity
+		@gender = gender
+		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		@age = 0
 	end
 
-	def speak 
+	def speak
 		p "Ho, ho, ho! Haaaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(type)
 		p "That was a good #{type} cookie!"
 	end
+
+#Getter Methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+#Setter Methods
+	def celebrate_birthday=(age)
+		@age = age
+	end
+
+	#Couldn't quite figure out how to get the reindeer to the -1 index of my [reindeer_ranking]
+	def get_mad_at=(reindeer_name)
+		index = @reindeer_ranking.find_index(reindeer_name)
+		#  @reindeer_ranking[index].rotate!(-1)
+		@reindeer_ranking.insert(index, @reindeer_ranking.delete_at(-1))
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
 end
-santa = []
-genders = ['male', 'female', 'transgender']
-ethnicities = ['caucasian', 'asian', 'african american']
- genders.length.times do |i|
- 	santa = Santa.new(gender[i], ethnicities[i])
- 	p santa
+
+
+santas = []
+gender = ['Male', 'Female', 'Transgender']
+ethnicity = ['Caucasian', 'Asian', 'African American']
+
+gender.each do |gender, ethnicity|
+ 	santas << Santa.new(gender, ethnicity)
  end
-checker = Santa.new(gender,ethnicity)
-checker.speak
-checker.eat_milk_and_cookies('snickerdoodle')
+ p santas[0] #Debugging call
+
+santas[0].celebrate_birthday =+ 1
+p santas[0]
+
+santas[0].gender = "Female"
+p santas[0]
+
+santas[0].get_mad_at = "Rudolph"
+p santas[0]
