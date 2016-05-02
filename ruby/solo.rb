aa55e2f869c7b011e116452d6ab6af898dbf1f20
Nick Block <nicholasblock78@gmail.com>
2016-05-02 01:39:39 -0500

    Finish 6.4 Release 2

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -15,16 +15,30 @@
 
 class Garden
   attr_reader
-  attr_accessor :num_plants, :num_flowers, :need_water
+  attr_accessor :num_plants, :num_flowers, :need_water, :gardens, :garden_name
 
   def initialize
-    puts "Initializing..."
-    @num_plants = rand(0..10)
-    @num_flowers = 0
-    @need_water = false
+    puts "Planting a new garden..."
+    puts "What would you like to name this garden?"
+    @garden_name = gets.chomp.capitalize
+    puts "Please enter how many plants you would like to start with:"
+    @num_plants = gets.chomp.to_i
+    puts "Please enter how many flowers would you like to start with:"
+    @num_flowers = gets.chomp.to_i
+    puts "Has it rained recently? (y/n)"
+      rain = gets.chomp.downcase
+      if rain == "y"
+        @need_water = false
+        puts "No need to water your plants then."
+      else
+        @need_water = true
+        puts "You better water your plants!"
+      end
   end
 
   def about
+    puts "-------------"
+    puts "Your garden name is #{@garden_name}"
     puts "You have #{@num_plants} plants"
     puts "Your plants have #{@num_flowers} flowers"
     if @need_water
@@ -32,6 +46,7 @@ class Garden
     else
       puts "Your plants do not need water."
     end
+    puts "-------------"
   end
 
   def water_plants
@@ -61,12 +76,41 @@ class Garden
 
 end
 
-nicks_garden = Garden.new
-nicks_garden.about
-puts "-------------"
-nicks_garden.add_plants(3)
-nicks_garden.drought
-nicks_garden.water_plants
-nicks_garden.bloom(2)
-puts "-------------"
-nicks_garden.about
+#Start of Control Flow of Program
+
+##Need to come back through and figure out this looping
+#None of my loops are starting/ending where they are supposed to...UGH!!!!!!
+
+
+puts "Welcome to my Garden program!"
+
+
+  @gardens = []
+loop do
+  puts "Would you like to create a garden? (y/n)"
+  input = gets.chomp.downcase
+
+
+  if input == 'n'
+    #Iterate over the array, printing out info for each garden created by user
+    @gardens.each do |garden|
+      garden.about
+    end
+    puts "Come back next time!"
+    break
+  end
+  @gardens << (@garden_name = Garden.new)
+end
+
+
+
+##Old Driver Code from Release 1
+# nicks_garden = Garden.new
+# nicks_garden.about
+# puts "-------------"
+# nicks_garden.add_plants(3)
+# nicks_garden.drought
+# nicks_garden.water_plants
+# nicks_garden.bloom(2)
+# puts "-------------"
+# nicks_garden.about
