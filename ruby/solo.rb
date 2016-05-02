eed894555de7483958b06a798fcd9fd52a0e3b8f
Nick Block <nicholasblock78@gmail.com>
2016-05-01 23:28:00 -0500

    6.4 Relase 0 - Pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+# Create a Garden Class
+#
+# 3 attributes --------------
+#   number of plants -fixnum
+#   number of flowers -fixnum
+#   need to water? -boolean
+#
+# 3 methods --------------
+#   water plants
+#   add new plants - take argument with how many
+#   harvest plants
