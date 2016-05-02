dadabd5d0a1133fc136ee0890874f7b6aa4cd52b
Nick Block <nicholasblock78@gmail.com>
2016-05-01 23:20:14 -0500

    Semi Finish 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,13 @@
 class Santa
+	attr_accessor :age, :gender, :celebrate_birthday, :get_mad_at, :about, :ethnicity
 
-	def initialize(gender, ethnicity)
+	def initialize(name, gender, ethnicity)
 		p "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
+		@name = name
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(0..140)
 	end
 
 	def speak
@@ -16,39 +18,51 @@ class Santa
 		p "That was a good #{type} cookie!"
 	end
 
-#Getter Methods
-	def age
-		@age
-	end
+	def about
+			# puts "Santa Number: #{i}"
+			puts "Santa's Name: #{@name}"
+			puts "Santa's Age: #{@age}"
+			puts "Santa's Gender: #{@gender}"
+			puts "Santa's Ethnicity: #{@ethnicity}"
+		end
 
-	def ethnicity
-		@ethnicity
-	end
 
-#Setter Methods
-	def celebrate_birthday=(age)
-		@age = age
-	end
+##Getter Methods
+	# def age
+	# 	@age
+	# end
+	#
+	# def ethnicity
+	# 	@ethnicity
+	# end
+
+##Setter Methods
+	# def celebrate_birthday=(age)
+	# 	@age = age
+	# end
 
 	#Couldn't quite figure out how to get the reindeer to the -1 index of my [reindeer_ranking]
-	def get_mad_at=(reindeer_name)
-		index = @reindeer_ranking.find_index(reindeer_name)
-		#  @reindeer_ranking[index].rotate!(-1)
-		@reindeer_ranking.insert(index, @reindeer_ranking.delete_at(-1))
-	end
+	# def get_mad_at=(reindeer_name)
+	# 	index = @reindeer_ranking.find_index(reindeer_name)
+	# 	#  @reindeer_ranking[index].rotate!(-1)
+	# 	@reindeer_ranking.insert(index, @reindeer_ranking.delete_at(-1))
+	# end
 
-	def gender=(new_gender)
-		@gender = new_gender
-	end
+	# def gender=(new_gender)
+	# 	@gender = new_gender
+	# end
 end
 
-
 santas = []
-gender = ['Male', 'Female', 'Transgender']
-ethnicity = ['Caucasian', 'Asian', 'African American']
+@name = ['Kris Kringle', 'Tim Allen', 'Bad Santa']
+@gender = ['Male', 'Female', 'Transgender']
+@ethnicity = ['Caucasian', 'Asian', 'African American']
 
-gender.each do |gender, ethnicity|
- 	santas << Santa.new(gender, ethnicity)
+
+#Something is wrong with my loop. I will come back to this when I have more time!!!
+#Slash I don't want to keep going down this rabbit hole right now. Spent way too much time on this as is. 
+@gender.each do |name, gender, ethnicity|
+ 	santas << Santa.new(@name, @gender, @ethnicity)
  end
  p santas[0] #Debugging call
 
@@ -60,3 +74,5 @@ p santas[0]
 
 santas[0].get_mad_at = "Rudolph"
 p santas[0]
+
+santas[0].about
