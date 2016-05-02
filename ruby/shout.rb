0933ca6320e4c1c89dcfe678d1141632aaa7a911
Nick Block <nicholasblock78@gmail.com>
2016-05-02 04:08:03 -0500

    Finish 6.5

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,32 @@
 module Shout
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
-  end
-  def sit
-    puts "great"
+  def yell_in_caps(words)
+    puts words.upcase
   end
 end
 
-class Hello
+class Parent
   include Shout
 end
 
-require_relative 'shout'
-example = Hello.new
-example.self.yelling_happily('HI')
+class Coach
+  include Shout
+end
 
-puts Hello.new.sit
+mom = Parent.new
+mom.yell_in_caps('go to bed!')
+
+football_coach = Coach.new
+football_coach.yell_in_caps('hit the weights!')
+# #Old Code
+# module Shout
+#   def self.yell_angrily(words)
+#     puts words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     puts words + "!!!" + " :)"
+#   end
+# end
+#
+# require_relative 'Shout'
+# Shout.yell_angrily('goodbye')
+# Shout.yelling_happily('hi')
