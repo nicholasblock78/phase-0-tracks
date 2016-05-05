a811cc87eb2829c40289b228ae56b7b9699b19ef
Nick Block <nicholasblock78@gmail.com>
2016-05-05 10:23:12 -0500

    Add stuff

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var horse_colors = ['blue', 'yellow', 'pink', 'red']
+var horse_names = ['Bob', 'Sam', 'Doge', 'Riley']
+
+horse_colors.push('green')
+horse_names.push('Richard')
+
+console.log(horse_colors)
+console.log(horse_names)
