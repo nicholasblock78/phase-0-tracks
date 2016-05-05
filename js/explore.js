90f22b79ef0c54fe73b816c73bd2f457fbf3f7b5
Nick Block <nicholasblock78@gmail.com>
2016-05-04 23:06:04 -0500

    Halfway thru programming 7.1

--- js/explore.js
+++ js/explore.js
@@ -8,14 +8,30 @@
 //split string into an array, then reverse array, then join back together into a string
 //Declaring the function
 
-//loop thru each char and save to array in backwords fashion -1, -2, -3 etc
+//loop thru each char and save to array in backwards fashion -1, -2, -3 etc
 //then add index to index in reverse order '[-1]' + '[-2]'
+
 function reverse(string) {
-  var split_string = string.split('');
-  var reversed_array = split_string.reverse();
-  var reversed_string = reversed_array.join('');
-  return reversed_string;
+  // To take each character and save into an array
+  var str_arr = []
+  for (var i=0, len = string.length; i < len; i++) {
+    // var str_arr = []
+    str_arr.push(string[i]);
+  }
+  // Take from array and add index one by one w loop
+  // for (var i=-1; len1 = str_arr.length ; i--) {
+  //    var reversed_string = str_arr[i] + str_arr[i-1]
+  //
+  // }
+
+  return str_arr
 }
+// function reverse(string) {
+//   var split_string = string.split('');
+//   var reversed_array = split_string.reverse();
+//   var reversed_string = reversed_array.join('');
+//   return reversed_string;
+// }
 
 //Calling the function to test
 console.log(reverse('hello'));
