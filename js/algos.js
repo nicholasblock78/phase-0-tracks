e99c780f6991d025da2c03e38fbfda752477d006
Nick Block <nicholasblock78@gmail.com>
2016-05-10 16:40:25 -0500

    Finish 7.3 pseudocode

--- js/algos.js
+++ js/algos.js
@@ -32,8 +32,13 @@ console.log(LongestWord(word_array));
 
 
 //RELEASE 1 --------------------------
-
+//Check two instances of a hash object and check if any of the key/value pairs are equivalent
 function findMatch (object1, object2) {
+  //Sort keys into an array of keys
+  //sort values into an array of values
+  //compare key array [0] to [1]
+  //compare value array [0] to [1]
+  //if either of those two conditions match, then function should return true
 
 }
 
@@ -42,7 +47,8 @@ console.log(findMatch())
 //RELEASE 2 --------------------------
 //Create x number of words based on input, funnel them into an array
 //generate random character/number off of Math module
-
+//generate a random number then use a .times loop (javascript equivalent) to add in a random char
+  //from a possible string that I set alphabet="a-z"
 
 function randArrGenerator (integer) {
  var returned_arr = []
