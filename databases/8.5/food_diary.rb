86a1ffa3d7768bf798a7e63f7217b5abf46e0773
Nick Block <nicholasblock78@gmail.com>
2016-05-23 07:12:11 -0500

    finish 8.5

--- databases/8.5/food_diary.rb
+++ databases/8.5/food_diary.rb
@@ -6,14 +6,14 @@ require 'faker'
 #Create the database
 db = SQLite3::Database.new("food_log.db")
 
-#Create 2 tables 1-users 2-foods
-# users = <<-SQL
-#   CREATE TABLE IF NOT EXISTS food_diary(
-#     id INT PRIMARY KEY,
-#     name VARCHAR(255),
-#     daily_goal INT
-#   )
-# SQL
+# Create 2 tables 1-users 2-foods
+create_users_tbl = <<-SQL
+  CREATE TABLE IF NOT EXISTS food_diary(
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    daily_goal INT
+  )
+SQL
 create_foods_tbl = <<-SQL
   CREATE TABLE IF NOT EXISTS foods (
     id INTEGER PRIMARY KEY,
@@ -21,12 +21,20 @@ create_foods_tbl = <<-SQL
     calories INT
   );
 SQL
-# food_diary_db.execute(users)
+db.execute(create_users_tbl)
 db.execute(create_foods_tbl)
 # db.execute("INSERT INTO foods (name, calories) VALUES ('Bob', 200)")
 def add_food(db, name, calories)
   db.execute("INSERT INTO foods (name, calories) VALUES (?, ?)", [name, calories])
 end
-##USER INTERFACE
-
 add_food(db, Faker::Name.name, rand(25..650))
+
+##USER INTERFACE
+puts "Welcome to my Food Diary program!"
+puts "Please enter your Name: (first and last please!)"
+user_name = gets.chomp
+puts "What meal would you like to enter in:"
+puts "Choose from Breakfast/Lunch/Dinner/Snack"
+meal = gets.chomp.capitalize
+#Then loop through each meal until done entering in information
+#store food info in a hash {food_item => calories}
